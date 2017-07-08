2317d2088b70d062240648c63dffbb3863d02899
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-07-08 13:38:50 -0600

    clase 16, el objeto this

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-function a(){
-    console.log("Funcion a");
-}
-
-a();
 
 
-//a las funciones se les puede agregar cualquier cosa que se le pueda agregar un objeto
+var persona = {
+    nombre: "Maria",
+    apellido: "dubon",
+    imprimirNombre: function(){
+        console.log("Nombre completo");
+        //console.log(nombre); // error de referencia salvo que esté declarado nombre como global
+        console.log(this.nombre);
+    },
+    direccion: {
+        pais: "México",
+        obtenerPais: function(){
+            console.log(this);
+            console.log("La dirección es " + this.pais );
+            //var nuevaDireccion = function(){
+            //    console.log(this); // este this hace referencia a window
+            //}
+            var self = this;
+            var nuevaDireccion = function(){
+                console.log(this);
+                console.log("La dirección es " + self.pais );
+            }
 
-a.nombre = "Maria";
-a.direccion = {
-    pais:"México",
-    ciudad: "Tepic",
-    edificio: {
-        piso: "8vo"
+            nuevaDireccion();
+        }
     }
 }
+
+persona.imprimirNombre();
+persona.direccion.obtenerPais();
