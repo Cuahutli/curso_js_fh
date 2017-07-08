7d6b8c73e0de12cd3cc12834644fbb4c25f66856
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-07-08 13:23:22 -0600

    clase  15, funciones de primera clase

--- app.js
+++ app.js
@@ -1,62 +1,17 @@
-// una función puede regresar primitivos, objetos, undefined, función, null
-
-function obtenerAleatorio() {
-    return Math.random();
+function a(){
+    console.log("Funcion a");
 }
 
-function obtenerNombre() {
-    return "Juan"
-}
+a();
 
-function esMayor05(p) {
-    if (obtenerAleatorio() > 0.5){
-        return true;
-    }else{
-        return false;
+
+//a las funciones se les puede agregar cualquier cosa que se le pueda agregar un objeto
+
+a.nombre = "Maria";
+a.direccion = {
+    pais:"México",
+    ciudad: "Tepic",
+    edificio: {
+        piso: "8vo"
     }
 }
-
-console.log(obtenerAleatorio() + 10); // se puede tratar como número por que retorna un entero
-
-
-console.log(obtenerNombre() + " Padilla");
-
-console.log(esMayor05());
-
-if(esMayor05()){
-    console.log("es mayor a 0.5");
-}else{
-    console.log("es menor a 0.5");
-}
-
-
-function crearPersona(nombre, apellido){
-    return {
-       nombre: nombre,
-       apellido: apellido,     
-    }
-}
-
-var persona = crearPersona("Maria", "Paz")
-console.log(persona);
-console.log(persona.nombre + " " + persona.apellido)
-
-
-
-function creaFuncion() {
-    return function(nombre){
-        console.log("Me creo" + " " + nombre);
-        return function(){
-            console.log("segunda función");
-        }
-    }
-}
-
-
-var nuevaFuncion = creaFuncion();
-
-nuevaFuncion(persona.nombre);
-
-var segundaFuncion = nuevaFuncion(persona.nombre);
-
-segundaFuncion();
