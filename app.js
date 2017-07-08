46ada71f11d11a20c3955f1043ab15fd7c545979
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-07-08 00:34:07 -0600

    clase 6 tipos primitivos y objetos

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-var nombre;
+var num = 10;
+var str = "texto";
+var bol = true;
+var und = undefined;
+var nul = null;
+
+console.log(num);
+
+num = str;
+console.log(num);
+
+console.log(bol)
+bol = 10;
+console.log(bol)
+
+var obj = {}; /* objeto vacío */
+
+/* declaración de un objeto, lo que lleva dentro son propiedades */
+var obj = {
+    numero: 10,
+    texto: "Nuevo Texto",
+    objHijo: {
+        nombre: "Cuahutli",
+        edad: 37,
+    }
+};
+
+console.log(obj)
