2159408df297a971b28d37ace59a100ab811df69
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-07-08 00:24:31 -0600

    clase 8, el orden de importación importa

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-function imprimir() {
-    for (var i=0; i < 8000; i++){
-        console.log("imprimio");
-    }
-}
-
-function presionoClick(params) {
-    console.log("Click en botón")
-}
-
-imprimir();
+var nombre;
