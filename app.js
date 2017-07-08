3ae631d3b317a809c734d7b36555725811ac5174
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-07-08 12:33:01 -0600

    clase 13, parametros para funciones, tipos de parametros que le podemos pasar; funciones anonimas; funciones explicitas

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
-var a = 30  
-function primeraFuncion() {
-  var a= 20;
-  console.log(a);
+// los parametros pueden ser objetos, variables primitivas u otras funciones
+//function imprimir(nombre, apellido) {
+//    console.log(nombre +  " " + apellido)
+//}
+
+//variable nombrada
+//var nom = "juan"
+
+// "Juan" se convierte en una variable anónima
+//imprimir("Juan") // Juan undefined  function imprimir (nombre, apellido)
+// imprimir("Juan", "Padilla")
+
+/*
+function imprimir(nombre, apellido) {
+    // if (apellido === undefined){
+    //     apellido = "xxx";
+    // }
+    //apellido = apellido || "xxx";
+   console.log(nombre +  " " + apellido);
 }
 
-console.log(a);
+imprimir("Juan", "Padilla");*/
 
-primeraFuncion();
+/*
+function imprimir(persona) {
+    console.log(persona);
+    console.log(persona.nombre + " " + persona.apellido)
+    persona.nombre = "Maria"
+}
 
-console.log(a);
+var persona = {
+    nombre:"Juan",
+    apellido: "Padilla"
+}
+imprimir(persona);
 
-//undefined
-console.log(primeraFuncion());
+console.log(persona);
+*/
 
-var x = primeraFuncion(); // si no le pongo los parentesis, no estoy invocando a la función, lo que retorna es la función misma
 
-//undefined
-console.log(x)
+// fn es una convención que indica que estamos mandando una función (se usa mucho en jquery)
+function imprimir(fn) {
+    fn();
+}
+
+var persona = {
+    nombre: "Juan",
+    apellido: "Padilla"
+}
+
+// funcion anónima
+imprimir(function () {
+    console.log("Funcion anonima");
+})
+
+//función explicita
+
+var miFuncion= function () {
+    console.log("funcion explicita")
+}
+
+imprimir(miFuncion);
