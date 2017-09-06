740a351541057f058701518197693ac4924c02c9
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-06 02:08:52 -0600

    clase 54 cajas de dialogo

---
+++ clase 54/app.js
@@ -0,0 +1,13 @@
+
+
+alert("Hola mundo");
+
+
+var acepto = confirm("estás seguro que deseas borrar algo?");
+console.log(acepto);
+
+var dato = prompt("Dame un valor", "cuah");
+console.log(dato);
+
+// no es recomendable usarlo
+// javascript no es asincrono, solo puede hacer una cosa a la vez.
